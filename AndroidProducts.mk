a073cea1d818daf9f93315fb63bd482ef421b5a3
kssrao13882 <kssrao13882@gmail.com>
2023-02-27 15:35:34 +0530

    RMX1971: ColtOS Bringup
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1971.mk
+    $(LOCAL_DIR)/colt_RMX1971.mk
+
+COMMON_LUNCH_CHOICES := \
+    colt_RMX1971-user \
+    colt_RMX1971-userdebug \
+    colt_RMX1971-eng
